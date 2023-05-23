b0262de75588d56b81ab6b94a4745965b29dab79
Saurabh <sourabh15072002@gmail.com>
2023-05-24 00:42:43 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "98c87d72945024e1da6595e0096e456a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
